a3fd038ccc71e0f73625aa7fac38e20d2c0b7108
Christian <chris20vitto@gmail.com>
2022-11-26 16:50:51 +0800

    Working on Carousel in Activities

--- frontend/src/screen components/Activities1.jsx
+++ frontend/src/screen components/Activities1.jsx
@@ -3,81 +3,87 @@ import { Typography, Box } from "@mui/material";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { CardMedia } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 
 export default function Activities1() {
     return (
-        <Box style={{paddingBottom: "6rem"}}>
+
+        <Box style={{ paddingBottom: "6rem" }}>
             <Typography variant="h3" style={{ textAlign: "center", paddingTop: "250px", fontFamily: "Times New Roman", letterSpacing: "10px" }}>
                 ACTIVITIES
             </Typography>
             <Typography variant="subtitle2" style={{ textAlign: "center", letterSpacing: "2px", paddingTop: "60px", fontFamily: "Arial Narrow" }}>
                 EXPERIENCES AT DRT HOUSE
             </Typography>
-            {/* Added Cards in the Activities */}
-            <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center", paddingTop: "80px", gap: "5rem" }}>
+            <Grid container spacing={12}>
+                <Grid xs={12}>
+                    {/* Added Cards in the Activities */}
+                    <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center", paddingTop: "80px", gap: "5rem" }}>
 
-                {/* DRT KIDS CARD */}
-                <Card sx={{ maxWidth: 345, borderRadius: 0 }}>
-                    <CardMedia
-                        component="img"
-                        alt="DRT KIDS"
-                        height="265"
-                        image="/static/images/activities-img/drtkids.jpg"
-                    />
-                    <CardContent>
-                        <Typography variant="h5" component="div" style={{ textAlign: "center", fontFamily: "Times New Roman" }}>
-                            DRT KIDS
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "1rem", fontSize: "12px" }}>
-                            Children will enjoy our complimentary activities at DRT House, designed to give
-                            them enjoyable place to play and keep them engaged
-                        </Typography>
-                    </CardContent>
-                </Card>
+                        {/* DRT KIDS CARD */}
+                        <Card sx={{ maxWidth: 345, borderRadius: 0 }}>
+                            <CardMedia
+                                component="img"
+                                alt="DRT KIDS"
+                                height="265"
+                                image="/static/images/activities-img/drtkids.jpg"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="div" style={{ textAlign: "center", fontFamily: "Times New Roman" }}>
+                                    DRT KIDS
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "1rem", fontSize: "12px" }}>
+                                    Children will enjoy our complimentary activities at DRT House, designed to give
+                                    them enjoyable place to play and keep them engaged
+                                </Typography>
+                            </CardContent>
+                        </Card>
 
-                {/* KARAOKE ON! CARD */}
-                <Card sx={{ maxWidth: 345, borderRadius: 0 }}>
-                    <CardMedia
-                        component="img"
-                        alt="DRT KIDS"
-                        height="265"
-                        image="/static/images/activities-img/karaoke.jpg"
-                    />
-                    <CardContent>
-                        <Typography variant="h5" component="div" style={{ textAlign: "center", fontFamily: "Times New Roman" }}>
-                            KARAOKE-ON!
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "1rem", fontSize: "12px" }}>
-                            Filipinos are one of the best singers in the world and their vacation is not complete without
-                            Karaoke! You can rent this Videoke and enjoy at DRT House!
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "2.5rem", fontSize: "12px" }}>
-                            Rent this Videoke in your stay for only ₱500.00
-                        </Typography>
-                    </CardContent>
-                </Card>
+                        {/* KARAOKE ON! CARD */}
+                        <Card sx={{ maxWidth: 345, borderRadius: 0 }}>
+                            <CardMedia
+                                component="img"
+                                alt="DRT KIDS"
+                                height="265"
+                                image="/static/images/activities-img/karaoke.jpg"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="div" style={{ textAlign: "center", fontFamily: "Times New Roman" }}>
+                                    KARAOKE-ON!
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "1rem", fontSize: "12px" }}>
+                                    Filipinos are one of the best singers in the world and their vacation is not complete without
+                                    Karaoke! You can rent this Videoke and enjoy at DRT House!
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "2.5rem", fontSize: "12px" }}>
+                                    Rent this Videoke in your stay for only ₱500.00
+                                </Typography>
+                            </CardContent>
+                        </Card>
 
-                {/* INFINITY POOL CARD */}
-                <Card sx={{ maxWidth: 345, borderRadius: 0 }}>
-                    <CardMedia
-                        component="img"
-                        alt="DRT KIDS"
-                        height="265"
-                        image="/static/images/activities-img/pool.jpg"
-                    />
-                    <CardContent>
-                        <Typography variant="h5" component="div" style={{ textAlign: "center", fontFamily: "Times New Roman" }}>
-                            INFINITY POOL
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "1rem", fontSize: "12px" }}>
-                            Plunge into DRT House Infinity pool and experience the greater view of 
-                            Doña Remedios Trinidad mountain view. Our pool experiences charm our guests 
-                            leaving them with endless reason to stay in the water.
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </div>
+                        {/* INFINITY POOL CARD */}
+                        <Card sx={{ maxWidth: 345, borderRadius: 0 }}>
+                            <CardMedia
+                                component="img"
+                                alt="DRT KIDS"
+                                height="265"
+                                image="/static/images/activities-img/pool.jpg"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" component="div" style={{ textAlign: "center", fontFamily: "Times New Roman" }}>
+                                    INFINITY POOL
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" style={{ textAlign: "center", paddingTop: "1rem", fontSize: "12px" }}>
+                                    Plunge into DRT House Infinity pool and experience the greater view of
+                                    Doña Remedios Trinidad mountain view. Our pool experiences charm our guests
+                                    leaving them with endless reason to stay in the water.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </Grid>
+            </Grid>
         </Box>
-        
+
     )
 }
--- frontend/src/screen components/Activities2.jsx
+++ frontend/src/screen components/Activities2.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import Grid from '@mui/material/Unstable_Grid2';
 
 export default function Activities2() {
     return (
@@ -10,6 +11,28 @@ export default function Activities2() {
             <Typography align='center' style={{ fontFamily: "Times New Roman", fontSize: "2.5rem", fontWeight: "bold", letterSpacing: "10%", paddingTop: "1.7rem" }}>
                 SEEK AND EXPLORE DRT’S BEST <br />NATURE GETAWAY
             </Typography>
+            <center>
+                <Grid container spacing={9}>
+
+                    <div style={{ paddingTop: "12rem", display: "flex", margin: "0 10rem 0 7rem" }}>
+                        <Grid lg={12}>
+                            <img src="/static/images/activities-img/pool.jpg" alt="Punsong Bato" width={"100%"} />
+                        </Grid>
+                        <Grid lg={8}>
+                            <div style={{ marginTop: "10rem" }}>
+                                <Typography variant="h4" style={{ fontFamily: "Times New Roman", fontWeight: "bold", textAlign: "left" }}>
+                                    PUNSONG BATO
+                                </Typography>
+                                <Typography variant="subtitle2" style={{ fontFamily: "Arial", textAlign: "left", marginTop: "5rem" }}>
+                                    One of the best river in Doña Remedios Trinidad with crystal clear water and beautiful rock formations.
+                                </Typography>
+                            </div>
+                        </Grid>
+
+                    </div>
+
+                </Grid>
+            </center>
         </div>
     )
 }
